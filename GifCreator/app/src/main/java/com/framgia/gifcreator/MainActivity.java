d0ab539fe54161ae77bbc228cc8be09e0d092096
Btnisme -github data <Btnisme123@gmail.com>
2016-06-02 11:09:43 +0700

    get photo from gallery

--- GifCreator/app/src/main/java/com/framgia/gifcreator/MainActivity.java
+++ GifCreator/app/src/main/java/com/framgia/gifcreator/MainActivity.java
@@ -1,52 +1,60 @@
 package com.framgia.gifcreator;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mButtonImageGallery;
+    private ImageView mImageGallery;
+    public static final int RESULT_LOAD_IMG = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        findView();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    private void findView() {
+        mButtonImageGallery = (Button) findViewById(R.id.button_imageGallery);
+        mImageGallery = (ImageView) findViewById(R.id.image_gallery);
+        mButtonImageGallery.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK && data != null) {
+            Uri selectedImg = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImg, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String imgDecodeString = cursor.getString(columnIndex);
+            cursor.close();
+            mImageGallery.setImageBitmap(BitmapFactory.decodeFile(imgDecodeString));
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_imageGallery:
+                openGalerry();
+                break;
+        }
+    }
+
+    private void openGalerry(){
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,RESULT_LOAD_IMG);
     }
 }
