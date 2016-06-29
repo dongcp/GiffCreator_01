66e31ff48d12c340c0a03077bf5c41a28b31dc11
Btnisme -github data <Btnisme123@gmail.com>
2016-06-29 10:11:43 +0700

    change way to remove image

--- GifCreator/app/src/main/java/com/framgia/gifcreator/ui/activity/ShowListChosenImageActivity.java
+++ GifCreator/app/src/main/java/com/framgia/gifcreator/ui/activity/ShowListChosenImageActivity.java
@@ -12,6 +12,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
